941dc8b2efbfacf4e7fe0535ff3398b18308a9a5
huytuan033 <hahuytuan033@gmail.com>
2024-04-15 11:26:15 +0700

    button

--- Assets/Scripts/GamePlay.cs
+++ Assets/Scripts/GamePlay.cs
@@ -4,17 +4,38 @@ using UnityEngine.UI;
 public class GamePlay : MonoBehaviour
 {
     public Image loadingBar;
-    public Button button1;
-    public Button button2;
-    public Button button3;
+    public Button hairButton;
+    public Button jacketButton;
+    public Button shoesButton;
+    public Button button4;
+    public Button button5;
+//cái được ẩn đi
+    public Image hair_image;
+    public Image jacket_image;
+    public Image shoes_image;
+
+//cái được hiện lên
+    public GameObject hair;
+    public GameObject jacket;
+    public GameObject shoes; // Add this line
     private int buttonClickCount = 0;
 
     private void Start()
     {
         loadingBar.fillAmount = 0f;
-        button1.onClick.AddListener(IncrementLoadingBar);
-        button2.onClick.AddListener(IncrementLoadingBar);
-        button3.onClick.AddListener(IncrementLoadingBar);
+        hairButton.onClick.AddListener(IncrementLoadingBar);
+        jacketButton.onClick.AddListener(()=>
+        {
+            IncrementLoadingBar();
+            jacket_image.gameObject.SetActive(false);
+            jacket.SetActive(true);
+        });
+        shoesButton.onClick.AddListener(() => 
+        {
+            IncrementLoadingBar();
+            shoes_image.gameObject.SetActive(false); // Disable button3
+            shoes.SetActive(true); // Enable otherObject
+        });
     }
 
     void IncrementLoadingBar()
